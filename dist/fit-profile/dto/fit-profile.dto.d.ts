65fa7e3f6263a5c4d2e3b7b97c331c391e7b2258
sanimuhammad09 <muhammadzenqeeth@gmail.com>
2026-07-18 19:24:44 +0100

    chore: remove dist from git to force railway build

--- dist/fit-profile/dto/fit-profile.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class SaveFitProfileDto {
-    gender: string;
-    heightCm: number;
-    weightKg: number;
-    bodyShape: string;
-    preferredFit: string;
-    brandSizeRef?: string;
-    ageRange?: string;
-}
