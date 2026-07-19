030c4ef0a7131343815e1920254fb590b5e5f160
sanimuhammad09 <muhammadzenqeeth@gmail.com>
2026-07-19 07:58:14 +0100

    fix: remove onModuleInit from PrismaService to prevent the application from hanging on boot due to database pooler timeouts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -9,10 +9,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     });
   }
 
-  async onModuleInit() {
-    await this.$connect();
-  }
-
   async onModuleDestroy() {
     await this.$disconnect();
   }
