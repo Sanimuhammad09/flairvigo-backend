ac25a48c3d70d3c4957e202331705945db901535
sanimuhammad09 <muhammadzenqeeth@gmail.com>
2026-08-04 02:44:31 +0100

    fix: exclude root route from global api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, RequestMethod } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
@@ -23,7 +23,9 @@ async function bootstrap() {
     });
 
     // Global prefix
-    app.setGlobalPrefix('api');
+    app.setGlobalPrefix('api', {
+      exclude: [{ path: '/', method: RequestMethod.GET }],
+    });
 
     // Validation
     app.useGlobalPipes(
